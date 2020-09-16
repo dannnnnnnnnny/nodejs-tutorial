864b799d82fd2eec64cde199c521789c3c759e34
dh_kim <ehdgnl5249@naver.com>
2020-09-16 13:36:36 +0900

    로그아웃 처리

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,34 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-export default function LandingPage() {
+export default function LandingPage(props) {
     
     useEffect(() => {
         axios.get('/api/hello')
         .then(res => {console.log(res)})
     }, [])
     
+    const onCLickHandler = () => {
+        axios.get('api/users/logout')
+            .then(response => {
+                if(response.data.success) {
+                    props.history.push('/login')
+                } else {
+                    alert('로그아웃 실패')
+                }
+            })
+    }
+
     return (
         <div style= {{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onCLickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
